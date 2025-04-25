474425c521d488712fcdf4e751aa9f31f6685a0f
binhex <megalith01@gmail.com>
2025-04-25 10:51:48 +0100

    force create paths for config and data

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/dumb-init /bin/bash
 
+syncthing_config_path='/config/syncthing/config'
+syncthing_data_path='/config/syncthing/data'
 config_filepath='/config/syncthing/config/config.xml'
+
+mkdir -p "${syncthing_config_path}" "${syncthing_data_path}"
 
 if [ -f "${config_filepath}" ]; then
 	# disable syncthing auto upgrade
@@ -8,4 +12,4 @@ if [ -f "${config_filepath}" ]; then
 fi
 
 # run syncthing
-/usr/bin/syncthing -config="/config/syncthing/config" -data="/config/syncthing/data" -gui-address="http://0.0.0.0:8384"
+/usr/bin/syncthing -config="${syncthing_config_path}" -data="${syncthing_data_path}" -gui-address="http://0.0.0.0:8384"
